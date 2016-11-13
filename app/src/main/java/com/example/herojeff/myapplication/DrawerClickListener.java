3aa4913304d53d4eac4aa6613e829b07205086db
herojeff02 <herojeff02@gmail.com>
2016-11-13 20:53:36 +0900

    Finishing app

--- app/src/main/java/com/example/herojeff/myapplication/DrawerClickListener.java
+++ app/src/main/java/com/example/herojeff/myapplication/DrawerClickListener.java
@@ -4,13 +4,14 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 
 /**
  * Created by herojeff on 2016. 9. 14..
  */
-public class DrawerClickListener implements AdapterView.OnItemClickListener{
+public class DrawerClickListener extends AppCompatActivity implements AdapterView.OnItemClickListener{
     Context mContext;
     MainActivity.Pac[] pacsForAdapter;
     PackageManager pmForListener;
@@ -22,13 +23,13 @@ public class DrawerClickListener implements AdapterView.OnItemClickListener{
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
-        MainActivity.appPos=pos;
         if(MainActivity.appLaunchable) {
             Intent launchIntent = new Intent(Intent.ACTION_MAIN);
             launchIntent.addCategory(Intent.CATEGORY_LAUNCHER);
             ComponentName cp = new ComponentName(pacsForAdapter[pos].packageName, pacsForAdapter[pos].name);
             launchIntent.setComponent(cp);
             mContext.startActivity(launchIntent);
+            //Toast.makeText(mContext, pacsForAdapter[pos].packageName + "  " + pacsForAdapter[pos].name, Toast.LENGTH_LONG).show();
         }
     }
 }
